a1cb657a567fa3f43119efdaacc2af058ad85837
ravula7 <ravula7@ad.unc.edu>
2022-04-29 16:54:31 -0400

    initial

--- public/main.js
+++ public/main.js
@@ -1,6 +1,23 @@
 // Focus div based on nav button click
 
+const { response } = require("express")
+
 // Flip one coin and show coin image to match result when button clicked
+const coin = document.getElementById("coin")
+coin.addEventListener("click", flipCoin)
+
+async function flipCoin(){
+    const endpoint="app/flip/"
+    const url = document.baseURI+endpoint
+    await fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(result){
+        console.log(result);
+        document.getElementById("result").innerHTML = result.flip;
+        //document.getElementById("quarter").setAttribute("src".)
+    }
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
