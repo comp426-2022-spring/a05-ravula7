4232fef1a7fb2167f25ef7f2f3a52b607cee2da3
ravula7 <ravula7@ad.unc.edu>
2022-04-30 09:06:18 -0400

    index.js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 // Focus div based on nav button click
 
-const { response } = require("express")
-
+const { response, application } = require("express");
 // Flip one coin and show coin image to match result when button clicked
 const coin = document.getElementById("coin")
 coin.addEventListener("click", flipCoin)
@@ -17,9 +16,118 @@ async function flipCoin(){
         console.log(result);
         document.getElementById("result").innerHTML = result.flip;
         //document.getElementById("quarter").setAttribute("src".)
-    }
+    });
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
+const coins = document.getElementById("coins")
+coins.addEventListener("submit", flipCoins)
+async function flipCoins(event){
+    event.preventDefault();
+    const endpoint = "app/flip/coins"
+    const url = document.baseURI+endpoint
+    const formEvent = event.currentTarget
+
+    try{
+        const formData = new FormData(formEvent);
+        const flips = await sendFlips({ url, formData});
+        console.log(flips)
+        document.getElementById("heads").innerHTML = "heads" + flips.summary.heads;
+        document.getElementById("tails").innerHTML = "tails" + flips.summary.tails;
+
+        document.getElementById("coinlist").innerHTML = coinList(flips.raw);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+const call = document.getElementById("call")
+call.addEventListener("submit", flipCall)
+async function flipCall(event){
+    event.preventDefault();
+    const endpoint = "app/flip/call/"
+    const url = document.baseURI+endpoint
+    const formEvent = event.currentTarget
+
+    try{
+        const formData = new FormData(formEvent);
+        const results = await sendFlips({ url, formData});
+        console.log(results);
+
+        document.getElementById("choice").innerHTML = "Guess: " + results.call;
+        document.getElementById("actual").innerHTML = "Actual: " + results.flip;
+        document.getElementById("results").innerHTML = "Result: " + results.result;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+async function sendFlips({ url, formData}){
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJson = JSON.stringify(plainFormData);
+    console.log(formDataJson);
+
+    const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json"
+		},
+		body: formDataJson
+	};
+
+    const response = await fetch(url, options);
+    return response.json()
+
+
+
+    //navigation
+    function homeNav() {
+        document.getElementById("homenav").className = "active";
+        document.getElementById("home").className = "active";
+        document.getElementById("singlenav").className = "";
+        document.getElementById("single").className = "inactive";
+        document.getElementById("multinav").className = "";
+        document.getElementById("multi").className = "inactive";
+        document.getElementById("guessnav").className = "";
+        document.getElementById("guesscoin").className = "inactive";
+      }
+      function singleNav() {
+        document.getElementById("homenav").className = "";
+        document.getElementById("home").className = "inactive";
+        document.getElementById("singlenav").className = "active";
+        document.getElementById("single").className = "active";
+        document.getElementById("multinav").className = "";
+        document.getElementById("multi").className = "inactive";
+        document.getElementById("guessnav").className = "";
+        document.getElementById("guesscoin").className = "inactive";
+      }
+      function multiNav() {
+        document.getElementById("homenav").className = "";
+        document.getElementById("home").className = "inactive";
+        document.getElementById("singlenav").className = "";
+        document.getElementById("single").className = "inactive";
+        document.getElementById("multinav").className = "active";
+        document.getElementById("multi").className = "active";
+        document.getElementById("guessnav").className = "";
+        document.getElementById("guesscoin").className = "inactive";
+      }
+      function guessNav() {
+        document.getElementById("homenav").className = "";
+        document.getElementById("home").className = "inactive";
+        document.getElementById("singlenav").className = "";
+        document.getElementById("single").className = "inactive";
+        document.getElementById("multinav").className = "";
+        document.getElementById("multi").className = "inactive";
+        document.getElementById("guessnav").className = "active";
+        document.getElementById("guesscoin").className = "active";
+      }
+
+
+
+
+
+}
+
 
 // Guess a flip by clicking either heads or tails button
